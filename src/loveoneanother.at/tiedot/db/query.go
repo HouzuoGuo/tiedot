f6c1044f8204d6ff5ec514fd15c30127051f552b
Howard Guo <guohouzuo@gmail.com>
2013-06-18 16:03:43 +1000

    query interpreter written

--- src/loveoneanother.at/tiedot/db/query.go
+++ src/loveoneanother.at/tiedot/db/query.go
@@ -2,30 +2,155 @@
 package db
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 )
 
 const ()
 
 /*
- */
 
-// Evaluate a query and return query result.
-func Eval(expr interface{}, src *Col) (result []uint64) {
-	fmt.Println("Eval", expr)
-	switch v := expr.(type) {
-	case nil:
-		return []uint64{}
-	case uint64:
-		return []uint64{v}
+Lookup:
+["=", {"eq": "the_value", "limit": 1, "in": ["path1", "path2"]}]
+
+Return all:
+["all"]
+
+intersect:
+["n", [], [], []]
+
+complement:
+["\", [], [], []]
+
+union:
+["u", [], [], []]
+
+*/
+
+// Evaluate a query and return query result. Use result map keys!
+func Eval(q interface{}, src *Col, result *map[uint64]bool) (err error) {
+	fmt.Println("Eval", q)
+	switch expr := q.(type) {
+	// 1, 2.0, etc
 	case float64:
-		return []uint64{uint64(v)}
+
+	// ["op", "param1", "param2"...]
 	case []interface{}:
-		result = make([]uint64, 0, 16)
-		for _, sub := range v {
-			result = append(result, Eval(sub, src)...)
+		switch op := expr[0].(type) {
+		// "op"
+		case string:
+			switch op {
+			// lookup
+			case "=":
+				switch lookupParam := expr[1].(type) {
+				case map[string]interface{}:
+					limit, hasLimit := lookupParam["limit"]
+					path, hasPath := lookupParam["in"]
+					lookupValue, hasEq := lookupParam["eq"]
+					if !(hasPath && hasEq) {
+						err = errors.New(fmt.Sprintf("expecting `in` vector and `eq` value"))
+						return
+					}
+					vecPath := make([]string, 0)
+					intLimit := uint64(0)
+					// figure out lookup path
+					if vecIfPath, ok := path.([]interface{}); ok {
+						for _, v := range vecIfPath {
+							vecPath = append(vecPath, fmt.Sprint(v))
+						}
+					} else {
+						return errors.New(fmt.Sprintf("expecting `in` vector, but you gave me: %v", path))
+
+					}
+					// figure out lookup limit
+					if hasLimit {
+						if floatLimit, ok := limit.(float64); ok {
+							intLimit = uint64(floatLimit)
+						} else {
+							return errors.New(fmt.Sprintf("expecting `limit` number, but you gave me: %v", floatLimit))
+						}
+					}
+					// now do lookup!
+					if ht, indexScan := src.StrHT[strings.Join(vecPath, ",")]; indexScan {
+						// index scan is much prefered
+						hashValue := StrHash(lookupValue)
+						_, vals := ht.Get(hashValue, intLimit, func(k, v uint64) bool {
+							// to avoid hash collision
+							for _, v := range GetIn(src.Read(v), vecPath) {
+								if v == lookupValue {
+									return true
+								}
+							}
+							return false
+						})
+						for _, docID := range vals {
+							(*result)[docID] = false
+						}
+					} else {
+						// fallback to collection scan
+						src.ForAll(func(id uint64, doc interface{}) {
+							for _, v := range GetIn(doc, vecPath) {
+								if v == lookupValue {
+									(*result)[id] = false
+									return
+								}
+							}
+						})
+					}
+				default:
+					return errors.New(fmt.Sprintf("expecting a map of lookup parameters, but you gave me: %v", lookupParam))
+				}
+			// intersect
+			case "n":
+				for _, subExpr := range expr[1 : len(expr)-1] {
+					subExprResult := make(map[uint64]bool)
+					intersection := make(map[uint64]bool)
+					err = Eval(subExpr, src, &subExprResult)
+					if err != nil {
+						return
+					}
+					// calculate intersection
+					for k, _ := range subExprResult {
+						if _, inBoth := (*result)[k]; inBoth {
+							intersection[k] = false
+						}
+					}
+					result = &intersection
+				}
+			// complement
+			case "\\":
+				for _, subExpr := range expr[1 : len(expr)-1] {
+					subExprResult := make(map[uint64]bool)
+					complement := make(map[uint64]bool)
+					err = Eval(subExpr, src, &subExprResult)
+					if err != nil {
+						return
+					}
+					// calculate intersection
+					for k, _ := range subExprResult {
+						if _, inBoth := (*result)[k]; !inBoth {
+							complement[k] = false
+						}
+					}
+					result = &complement
+				}
+			// union
+			case "u":
+				for _, subExpr := range expr[1 : len(expr)-1] {
+					err = Eval(subExpr, src, result)
+					if err != nil {
+						return
+					}
+				}
+			// all documents
+			case "all":
+				_, ids := src.IdIndex.GetAll()
+				for _, id := range ids {
+					(*result)[id] = false
+				}
+			}
 		}
-	case map[string]interface{}:
 	}
-	return result
+	return nil
 }
