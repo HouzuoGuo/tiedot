8854a86b1a24b8f9ea2430b79edbcd6147defed4
Howard Guo <guohouzuo@gmail.com>
2013-10-01 09:03:45 +1000

    Revert "remove per-document lock, it does not help improving performance"
    This reverts commit 0593cff5db087c1f9296c8219b65eeb053d79b8c.

--- file/col.go
+++ file/col.go
@@ -18,10 +18,11 @@ const (
 )
 
 type ColFile struct {
-	File          *File
-	Padding       []byte
-	syncDocInsert *sync.Mutex
-	syncDocUpdate *sync.RWMutex
+	File           *File
+	Padding        []byte
+	syncDocInsert  *sync.Mutex
+	syncLockCreate *sync.RWMutex
+	syncDocUpdate  map[uint64]*sync.RWMutex
 }
 
 // Open a collection file.
@@ -32,7 +33,28 @@ func OpenCol(name string) (*ColFile, error) {
 	for i := range padding {
 		padding[i] = 32
 	}
-	return &ColFile{File: file, Padding: padding, syncDocInsert: new(sync.Mutex), syncDocUpdate: new(sync.RWMutex)}, err
+	return &ColFile{File: file, Padding: padding, syncDocInsert: new(sync.Mutex), syncLockCreate: new(sync.RWMutex), syncDocUpdate: make(map[uint64]*sync.RWMutex)}, err
+}
+
+// Return document's own RWMutex, and create one if it does not yet exist.
+func (col *ColFile) getDocMutex(bucket uint64) (mutex *sync.RWMutex) {
+	var ok bool
+	// the lock may not exist yet, if so we have to create it
+	col.syncLockCreate.RLock()
+	mutex, ok = col.syncDocUpdate[bucket]
+	col.syncLockCreate.RUnlock()
+	if !ok {
+		col.syncLockCreate.Lock()
+		col.syncDocUpdate[bucket] = new(sync.RWMutex)
+		col.syncLockCreate.Unlock()
+		col.syncLockCreate.RLock()
+		mutex, ok = col.syncDocUpdate[bucket]
+		col.syncLockCreate.RUnlock()
+		if !ok {
+			panic("this should not happen, document lock was created but then disappeared")
+		}
+	}
+	return
 }
 
 // Retrieve document data given its ID.
@@ -40,14 +62,15 @@ func (col *ColFile) Read(id uint64) []byte {
 	if id < 0 || id >= col.File.Append || col.File.Buf[id] != DOC_VALID {
 		return nil
 	}
-	col.syncDocUpdate.RLock()
+	mutex := col.getDocMutex(id)
+	mutex.RLock()
 	if room, _ := binary.Uvarint(col.File.Buf[id+1 : id+11]); room > DOC_MAX_ROOM {
-		col.syncDocUpdate.RUnlock()
+		mutex.RUnlock()
 		return nil
 	} else {
 		docCopy := make([]byte, room)
 		copy(docCopy, col.File.Buf[id+DOC_HEADER:id+DOC_HEADER+room])
-		col.syncDocUpdate.RUnlock()
+		mutex.RUnlock()
 		return docCopy
 	}
 }
@@ -62,7 +85,19 @@ func (col *ColFile) Insert(data []byte) (id uint64, err error) {
 	col.syncDocInsert.Lock()
 	id = col.File.Append
 	// when file is full, we have lots to do
-	col.File.CheckSizeAndEnsure(DOC_HEADER + room)
+	if !col.File.CheckSize(DOC_HEADER + room) {
+		// do not allow more locks to be created, because we have to lock them all
+		col.syncLockCreate.Lock()
+		// lock down everything before growing document data file (which *deletes* the memory buffer)
+		for _, v := range col.syncDocUpdate {
+			v.Lock()
+		}
+		col.File.CheckSizeAndEnsure(DOC_HEADER + room)
+		for _, v := range col.syncDocUpdate {
+			v.Unlock()
+		}
+		col.syncLockCreate.Unlock()
+	}
 	col.File.Buf[id] = 1
 	binary.PutUvarint(col.File.Buf[id+1:id+DOC_HEADER], room)
 	copy(col.File.Buf[id+DOC_HEADER:id+DOC_HEADER+len64], data)
@@ -78,18 +113,19 @@ func (col *ColFile) Update(id uint64, data []byte) (uint64, error) {
 		return id, errors.New(fmt.Sprintf("Document %d does not exist in %s", id, col.File.Name))
 	}
 	len64 := uint64(len(data))
-	col.syncDocUpdate.Lock()
+	mutex := col.getDocMutex(id)
+	mutex.Lock()
 	if room, _ := binary.Uvarint(col.File.Buf[id+1 : id+11]); room > DOC_MAX_ROOM {
-		col.syncDocUpdate.Unlock()
+		mutex.Unlock()
 		return id, errors.New(fmt.Sprintf("Document %d does not exist in %s", id, col.File.Name))
 	} else {
 		if len64 <= room { // overwrite
 			copy(col.File.Buf[id+DOC_HEADER:id+DOC_HEADER+len64], data[:])
 			copy(col.File.Buf[id+DOC_HEADER+len64:id+DOC_HEADER+room], col.Padding[0:room-len64])
-			col.syncDocUpdate.Unlock()
+			mutex.Unlock()
 			return id, nil
 		}
-		col.syncDocUpdate.Unlock()
+		mutex.Unlock()
 		// re-insert
 		col.Delete(id)
 		return col.Insert(data)
@@ -110,11 +146,12 @@ func (col *ColFile) ForAll(fun func(id uint64, doc []byte) bool) {
 		if addr >= col.File.Append {
 			break
 		}
-		col.syncDocUpdate.RLock()
+		mutex := col.getDocMutex(addr)
+		mutex.RLock()
 		validity := col.File.Buf[addr]
 		room, _ := binary.Uvarint(col.File.Buf[addr+1 : addr+11])
 		if validity != DOC_VALID && validity != DOC_INVALID || room > DOC_MAX_ROOM {
-			col.syncDocUpdate.RUnlock()
+			mutex.RUnlock()
 			log.Printf("In %s at %d, the document is corrupted\n", col.File.Name, addr)
 			// skip corrupted document
 			addr++
@@ -124,10 +161,10 @@ func (col *ColFile) ForAll(fun func(id uint64, doc []byte) bool) {
 			continue
 		}
 		if validity == DOC_VALID && !fun(addr, col.File.Buf[addr+DOC_HEADER:addr+DOC_HEADER+room]) {
-			col.syncDocUpdate.RUnlock()
+			mutex.RUnlock()
 			break
 		}
-		col.syncDocUpdate.RUnlock()
+		mutex.RUnlock()
 		addr += DOC_HEADER + room
 	}
 }
