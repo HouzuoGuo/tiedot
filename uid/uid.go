7e22039532ce915dc7ea9d02870d6673af7b318b
Howard Guo <guohouzuo@gmail.com>
2013-10-06 20:06:13 +1100

    code comments in uid

--- uid/uid.go
+++ uid/uid.go
@@ -5,8 +5,12 @@ import (
 	"encoding/hex"
 )
 
+// Generate and return a new UID (Unique IDentifier).
 func NextUID() string {
 	uid := make([]byte, 16)
+	// use Golang standard lib to get cryptographically secure pseudorandom numbers
+	// the generator is backed by /dev/urandom (as per Golang's implementation)
+	// be aware: this will be slow in Plan9 due to its lack of /dev/random
 	entropy, err := rand.Read(uid)
 	if err != nil || entropy != 16 {
 		panic("no enough entropy")
