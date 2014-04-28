580764119242893c5a74455e560f461ad71f670f
HouzuoGuo <guohouzuo@gmail.com>
2014-04-28 16:15:41 +0200

    file cleanup

--- uid/uid.go
+++ uid/uid.go
@@ -1,28 +1,8 @@
 package uid
 
-import (
-	"math/rand"
-	"strconv"
-)
-
-const (
-	PK_NAME = "@id" // Name of UID (PK) attribute
-)
+import "math/rand"
 
 // Generate and return a new UID (Unique IDentifier).
 func NextUID() uint64 {
 	return uint64(rand.Int63()) + uint64(rand.Int63())
-}
-
-// Return value of the PK attribute in the document.
-func PKOfDoc(doc map[string]interface{}) (uid uint64, found bool) {
-	docPK, ok := doc[PK_NAME].(string)
-	if !ok {
-		return 0, false
-	}
-	uid, err := strconv.ParseUint(docPK, 10, 64)
-	if err != nil {
-		return 0, false
-	}
-	return uid, true
 }
--- uid/uid_test.go
+++ uid/uid_test.go
@@ -1,9 +1,6 @@
 package uid
 
-import (
-	"encoding/json"
-	"testing"
-)
+import "testing"
 
 func TestNextUID(t *testing.T) {
 	for i := 0; i < 1000; i++ {
@@ -11,30 +8,5 @@ func TestNextUID(t *testing.T) {
 		if next == 0 {
 			t.Fatal(next)
 		}
-	}
-}
-
-func TestGetPKOfDoc(t *testing.T) {
-	var doc1, doc2, doc3 map[string]interface{}
-	json.Unmarshal([]byte(`{"@id": 1}`), &doc1)
-	json.Unmarshal([]byte(`{"@id": "a"}`), &doc2)
-	json.Unmarshal([]byte(`{"@id": "1"}`), &doc3)
-
-	if _, found := PKOfDoc(doc1); found {
-		t.Fatal(doc1)
-	}
-
-	if _, found := PKOfDoc(doc2); found {
-		t.Fatal(doc2)
-	}
-
-	if uid, found := PKOfDoc(doc3); !found || uid != 1 {
-		t.Fatal(doc3)
-	}
-}
-
-func BenchmarkUID(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		NextUID()
 	}
 }
