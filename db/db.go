48632013f75767231852023a76309d8be1833919
HouzuoGuo <howard.guo@numberfour.eu>
2015-01-29 18:16:28 +0100

    merge db source files

--- db/db.go
+++ db/db.go
@@ -1,8 +1,12 @@
+/* Single-shard - document management and index maintenance features */
 /* Single shard - collection and DB storage management features. */
+/* Single shard - collection schema and index management features. */
+/* Features supporting multi-shard environment in addition to the ordinary single-shard implementation. */
 package db
 
 import (
 	"fmt"
+	"github.com/HouzuoGuo/tiedot/data"
 	"github.com/HouzuoGuo/tiedot/tdlog"
 	"io"
 	"io/ioutil"
@@ -11,8 +15,55 @@ import (
 	"path"
 	"path/filepath"
 	"sort"
+	"strings"
 	"time"
 )
+
+const (
+	// Name of collection data file
+	DOC_DATA_FILE = "dat"
+	// Name of document ID lookup hash table
+	DOC_LOOKUP_FILE = "id"
+	// Index path separator (appears in hash table file name)
+	INDEX_PATH_SEP = "!"
+)
+
+// Resolve the attribute(s) in the document structure along the given path.
+func GetIn(doc interface{}, path []string) (ret []interface{}) {
+	docMap, ok := doc.(map[string]interface{})
+	if !ok {
+		return
+	}
+	var thing interface{} = docMap
+	// Get into each path segment
+	for i, seg := range path {
+		if aMap, ok := thing.(map[string]interface{}); ok {
+			thing = aMap[seg]
+		} else if anArray, ok := thing.([]interface{}); ok {
+			for _, element := range anArray {
+				ret = append(ret, GetIn(element, path[i:])...)
+			}
+			return ret
+		} else {
+			return nil
+		}
+	}
+	switch thing.(type) {
+	case []interface{}:
+		return append(ret, thing.([]interface{})...)
+	default:
+		return append(ret, thing)
+	}
+}
+
+// Hash a string using sdbm algorithm.
+func StrHash(str string) uint64 {
+	var hash uint64
+	for _, c := range str {
+		hash = uint64(c) + (hash << 6) + (hash << 16) - hash
+	}
+	return hash
+}
 
 // Single-shard database features.
 type DB struct {
@@ -186,4 +237,190 @@ func (db *DB) Dump(dest string) error {
 		return nil
 	}
 	return filepath.Walk(db.path, cpFun)
+}
+
+// Schema information and document/index management features for a single DB shard.
+type Col struct {
+	db   *DB
+	name string
+	// Document data
+	part *data.Partition
+	// Joint index paths VS hash table
+	hts map[string]*data.HashTable
+	// Joint index paths VS split index paths
+	indexPaths map[string][]string
+	// Currently held document locks
+	locked map[uint64]struct{}
+}
+
+// Open a collection and load schema information.
+func OpenCol(db *DB, name string) (*Col, error) {
+	col := &Col{db: db, name: name, locked: make(map[uint64]struct{})}
+	if err := os.MkdirAll(path.Join(col.db.path, col.name), 0700); err != nil {
+		return nil, err
+	}
+	return col, col.load()
+}
+
+// Load collection schema (index schema).
+func (col *Col) load() error {
+	col.hts = make(map[string]*data.HashTable)
+	col.indexPaths = make(map[string][]string)
+	// Open document collection
+	var err error
+	if col.part, err = data.OpenPartition(
+		path.Join(col.db.path, col.name, DOC_DATA_FILE),
+		path.Join(col.db.path, col.name, DOC_LOOKUP_FILE)); err != nil {
+		return err
+	}
+	// Search for index files
+	colDirContent, err := ioutil.ReadDir(path.Join(col.db.path, col.name))
+	if err != nil {
+		return err
+	}
+	for _, htFile := range colDirContent {
+		if htFile.IsDir() || htFile.Name() == DOC_DATA_FILE || htFile.Name() == DOC_LOOKUP_FILE {
+			continue
+		}
+		// Open index file
+		idxName := htFile.Name()
+		idxPath := strings.Split(idxName, INDEX_PATH_SEP)
+		col.indexPaths[idxName] = idxPath
+		if col.hts[idxName], err = data.OpenHashTable(
+			path.Join(col.db.path, col.name, idxName)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Close all collection files. Stop using the collection after the call.
+func (col *Col) close() error {
+	errs := make([]error, 0, 0)
+	if err := col.part.Close(); err != nil {
+		errs = append(errs, err)
+	}
+	for _, ht := range col.hts {
+		if err := ht.Close(); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	if len(errs) == 0 {
+		return nil
+	}
+	return fmt.Errorf("%v", errs)
+}
+
+// Partition documents into roughly equally sized portions, and run the function on each document in the portion.
+func (col *Col) ForEachDocInPage(page, total uint64, fun func(id uint64, doc []byte) bool) {
+	if !col.part.ForEachDoc(page, total, fun) {
+		return
+	}
+}
+
+// Return all indexed paths.
+func (col *Col) AllIndexes() (ret [][]string) {
+	ret = make([][]string, 0, len(col.indexPaths))
+	for _, path := range col.indexPaths {
+		pathCopy := make([]string, len(path))
+		for i, p := range path {
+			pathCopy[i] = p
+		}
+		ret = append(ret, pathCopy)
+	}
+	return ret
+}
+
+// Return all indexed paths. Index path segments are joint and return value is sorted.
+func (col *Col) AllIndexesJointPaths() (ret []string) {
+	ret = make([]string, 0, 0)
+	for _, path := range col.AllIndexes() {
+		ret = append(ret, strings.Join(path, INDEX_PATH_SEP))
+	}
+	sort.Strings(ret)
+	return ret
+}
+
+// Remove an index.
+func (col *Col) Unindex(idxPath []string) (err error) {
+	idxName := strings.Join(idxPath, INDEX_PATH_SEP)
+	if _, exists := col.indexPaths[idxName]; !exists {
+		return fmt.Errorf("Path %v is not indexed", idxPath)
+	}
+	delete(col.indexPaths, idxName)
+	col.hts[idxName].Close()
+	delete(col.hts, idxName)
+	err = os.RemoveAll(path.Join(col.db.path, col.name, idxName))
+	return
+}
+
+// Return reference to hash table.
+func (col *Col) MultiShardUseHT(jointPath string) *data.HashTable {
+	ret := col.hts[jointPath]
+	return ret
+}
+
+// Place a document lock by memorizing its ID in an internal structure.
+func (col *Col) MultiShardLockDoc(id uint64) error {
+	if _, locked := col.locked[id]; locked {
+		return fmt.Errorf("Document %d is currently locked", id)
+	}
+	col.locked[id] = struct{}{}
+	return nil
+}
+
+// Remove a document ID from the internal lock structure.
+func (col *Col) MultiShardUnlockDoc(id uint64) {
+	delete(col.locked, id)
+}
+
+// Insert a new document and immediately place a lock on it.
+func (col *Col) MultiShardLockDocAndInsert(id uint64, doc []byte) (err error) {
+	if err = col.MultiShardLockDoc(id); err != nil {
+		return err
+	}
+	_, err = col.part.Insert(id, doc)
+	return
+}
+
+// Retrieve a document by ID, return raw document content.
+func (col *Col) BPRead(id uint64) (doc []byte, err error) {
+	doc, err = col.part.Read(id)
+	return
+}
+
+// Retrieve a document by ID and immediately place a lock on it.
+func (col *Col) MultiShardLockDocAndRead(id uint64) (doc []byte, err error) {
+	if err = col.MultiShardLockDoc(id); err != nil {
+		return
+	}
+	doc, err = col.part.Read(id)
+	return
+}
+
+// Update a document by ID.
+func (col *Col) BPUpdate(id uint64, newDoc []byte) error {
+	return col.part.Update(id, newDoc)
+}
+
+// Delete a document by ID.
+func (col *Col) BPDelete(id uint64) {
+	col.part.Delete(id)
+}
+
+// Return approximate number of documents in the partition.
+func (col *Col) BPApproxDocCount() uint64 {
+	return col.part.ApproxDocCount()
+}
+
+// Install an index without re-indexing any document.
+func (col *Col) BPIndex(idxPath []string) (err error) {
+	idxName := strings.Join(idxPath, INDEX_PATH_SEP)
+	if _, exists := col.indexPaths[idxName]; exists {
+		return fmt.Errorf("Path %v is already indexed", idxPath)
+	}
+	col.indexPaths[idxName] = idxPath
+	idxFileName := path.Join(col.db.path, col.name, idxName)
+	col.hts[idxName], err = data.OpenHashTable(idxFileName)
+	return
 }
--- db/db_test.go
+++ db/db_test.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"os"
 	"path"
@@ -209,5 +210,77 @@ func TestDumpDB(t *testing.T) {
 	}
 	if err := db2.Close(); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestStrHash(t *testing.T) {
+	strings := []string{"", " ", "abc", "123"}
+	hashes := []uint64{0, 32, 417419622498, 210861491250}
+	for i := range strings {
+		if StrHash(strings[i]) != hashes[i] {
+			t.Fatalf("Hash of %s equals to %d, it should equal to %d", strings[i], StrHash(strings[i]), hashes[i])
+		}
+	}
+}
+
+func TestGetIn(t *testing.T) {
+	var obj interface{}
+	// Get inside a JSON object
+	json.Unmarshal([]byte(`{"a": {"b": {"c": 1}}}`), &obj)
+	if val, ok := GetIn(obj, []string{"a", "b", "c"})[0].(float64); !ok || val != 1 {
+		t.Fatal()
+	}
+	// Get inside a JSON array
+	json.Unmarshal([]byte(`{"a": {"b": {"c": [1, 2, 3]}}}`), &obj)
+	if val, ok := GetIn(obj, []string{"a", "b", "c"})[0].(float64); !ok || val != 1 {
+		t.Fatal()
+	}
+	if val, ok := GetIn(obj, []string{"a", "b", "c"})[1].(float64); !ok || val != 2 {
+		t.Fatal()
+	}
+	if val, ok := GetIn(obj, []string{"a", "b", "c"})[2].(float64); !ok || val != 3 {
+		t.Fatal()
+	}
+	// Get inside JSON objects contained in JSON array
+	json.Unmarshal([]byte(`{"a": [{"b": {"c": [1]}}, {"b": {"c": [2, 3]}}]}`), &obj)
+	if val, ok := GetIn(obj, []string{"a", "b", "c"})[0].(float64); !ok || val != 1 {
+		t.Fatal()
+	}
+	if val, ok := GetIn(obj, []string{"a", "b", "c"})[1].(float64); !ok || val != 2 {
+		t.Fatal()
+	}
+	if val, ok := GetIn(obj, []string{"a", "b", "c"})[2].(float64); !ok || val != 3 {
+		t.Fatal()
+	}
+	// Get inside a JSON array and fetch attributes from array elements, which are JSON objects
+	json.Unmarshal([]byte(`{"a": [{"b": {"c": [4]}}, {"b": {"c": [5, 6]}}], "d": [0, 9]}`), &obj)
+	if val, ok := GetIn(obj, []string{"a", "b", "c"})[0].(float64); !ok || val != 4 {
+		t.Fatal()
+	}
+	if val, ok := GetIn(obj, []string{"a", "b", "c"})[1].(float64); !ok || val != 5 {
+		t.Fatal()
+	}
+	if val, ok := GetIn(obj, []string{"a", "b", "c"})[2].(float64); !ok || val != 6 {
+		t.Fatal()
+	}
+	if len(GetIn(obj, []string{"a", "b", "c"})) != 3 {
+		t.Fatal()
+	}
+	if val, ok := GetIn(obj, []string{"d"})[0].(float64); !ok || val != 0 {
+		t.Fatal()
+	}
+	if val, ok := GetIn(obj, []string{"d"})[1].(float64); !ok || val != 9 {
+		t.Fatal()
+	}
+	if len(GetIn(obj, []string{"d"})) != 2 {
+		t.Fatal()
+	}
+	// Another example
+	json.Unmarshal([]byte(`{"a": {"b": [{"c": 2}]}, "d": 0}`), &obj)
+	if val, ok := GetIn(obj, []string{"a", "b", "c"})[0].(float64); !ok || val != 2 {
+		t.Fatal()
+	}
+	if len(GetIn(obj, []string{"a", "b", "c"})) != 1 {
+		t.Fatal()
 	}
 }
