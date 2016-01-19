bfa727312772c077dd70ebbe97123230ec3d9df9
Mauro Toffanin <toffanin.mauro@gmail.com>
2016-01-19 22:45:21 +0100

    Apply gofmt

--- main.go
+++ main.go
@@ -118,7 +118,7 @@ func main() {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt)
 		go func() {
-			for _ = range c {
+			for range c {
 				pprof.Lookup("goroutine").WriteTo(os.Stderr, 1)
 			}
 		}()
