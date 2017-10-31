76d22cde0d3d1626d5d799e27d0f2ad2b3aa8454
agoalofalife <agoalofalife@gmail.com>
2017-10-31 18:27:34 +0300

    add test methods all rename and refactoring functions setUp and tearDown

--- httpapi/collection_test.go
+++ httpapi/collection_test.go
@@ -11,25 +11,32 @@ import (
 
 var (
 	// list api request
-	requestCreteError = "http://localhost:8080/create"
-	requestCreate     = fmt.Sprintf("http://localhost:8080/create?col=%s", collection)
+	requestCreteError       = "http://localhost:8080/create"
+	requestCreate           = fmt.Sprintf("http://localhost:8080/create?col=%s", collection)
+	requestAll              = "http://localhost:8080/all"
+	requestRename           = fmt.Sprintf("http://localhost:8080/rename?old=%s&new=%s", collection, collectionNew)
+	requestRenameMissingOld = fmt.Sprintf("http://localhost:8080/rename?new=%s", collectionNew)
+	requestRenameMissingNew = fmt.Sprintf("http://localhost:8080/rename?old=%s", collection)
 
-	collection = "Feeds"
-	tempDir    = "./tmp"
+	collection    = "Feeds"
+	collectionNew = "Points"
+	tempDir       = "./tmp"
 )
 
-// setUp tests
-func TestMain(m *testing.M) {
+// setUp and tearDown
+func setupTestCase() {
 	if err := os.MkdirAll(tempDir, 0700); err != nil {
 		log.Println(err)
 	}
-	retCode := m.Run()
-
+}
+func tearDownTestCase() {
 	os.RemoveAll(tempDir)
-	os.Exit(retCode)
 }
 
+// Test Create
 func TestCreateError(t *testing.T) {
+	setupTestCase()
+	defer tearDownTestCase()
 	req := httptest.NewRequest("GET", requestCreteError, nil)
 	w := httptest.NewRecorder()
 	Create(w, req)
@@ -37,22 +44,28 @@ func TestCreateError(t *testing.T) {
 		t.Error("Expected return code 400 and error message")
 	}
 }
-
 func TestCreateDuplicateCollection(t *testing.T) {
+	setupTestCase()
+	defer tearDownTestCase()
 	req := httptest.NewRequest("GET", requestCreate, nil)
 	w := httptest.NewRecorder()
+	wDubl := httptest.NewRecorder()
 
 	var err error
 	if HttpDB, err = db.OpenDB(tempDir); err != nil {
 		panic(err)
 	}
 	Create(w, req)
-	Create(w, req)
-	if w.Code != 400 && w.Body.String() != fmt.Sprintf("Collection %s already exists", collection) {
+	Create(wDubl, req)
+
+	if wDubl.Code != 400 && wDubl.Body.String() != fmt.Sprintf("Collection %s already exists", collection) {
 		t.Error("Expected code 400 if collection create duplicate")
 	}
 }
+
 func TestCreate(t *testing.T) {
+	setupTestCase()
+	defer tearDownTestCase()
 	req := httptest.NewRequest("GET", requestCreate, nil)
 	w := httptest.NewRecorder()
 
@@ -63,5 +76,89 @@ func TestCreate(t *testing.T) {
 	Create(w, req)
 	if w.Code != 201 {
 		t.Error("Expected code 201 after call method Create")
+	}
+}
+
+// Test All
+func TestAll(t *testing.T) {
+	setupTestCase()
+	defer tearDownTestCase()
+	reqCreate := httptest.NewRequest("GET", requestCreate, nil)
+	reqAll := httptest.NewRequest("GET", requestAll, nil)
+	w := httptest.NewRecorder()
+
+	var err error
+	if HttpDB, err = db.OpenDB(tempDir); err != nil {
+		panic(err)
+	}
+	Create(w, reqCreate)
+	All(w, reqAll)
+	if w.Code != 201 && w.Body.String() != fmt.Sprintf("[\"%s\"]", collection) {
+		t.Error("Expected lists collection and status 201")
+	}
+}
+
+// Test Rename
+func TestRename(t *testing.T) {
+	setupTestCase()
+	defer tearDownTestCase()
+	reqCreate := httptest.NewRequest("GET", requestCreate, nil)
+	reqRename := httptest.NewRequest("GET", requestRename, nil)
+	reqAll := httptest.NewRequest("GET", requestAll, nil)
+
+	w := httptest.NewRecorder()
+	wRename := httptest.NewRecorder()
+	wAll := httptest.NewRecorder()
+
+	var err error
+	if HttpDB, err = db.OpenDB(tempDir); err != nil {
+		panic(err)
+	}
+	Create(w, reqCreate)
+	Rename(wRename, reqRename)
+	All(wAll, reqAll)
+
+	if wRename.Code != 200 && wAll.Body.String() != fmt.Sprintf("[\"%s\"]", collectionNew) {
+		t.Error("Expected code 200 after rename and rename collection")
+	}
+}
+func TestRenameMissingOldParameter(t *testing.T) {
+	setupTestCase()
+	defer tearDownTestCase()
+	reqCreate := httptest.NewRequest("GET", requestCreate, nil)
+	reqRename := httptest.NewRequest("GET", requestRenameMissingOld, nil)
+
+	w := httptest.NewRecorder()
+	wRename := httptest.NewRecorder()
+
+	var err error
+	if HttpDB, err = db.OpenDB(tempDir); err != nil {
+		panic(err)
+	}
+	Create(w, reqCreate)
+	Rename(wRename, reqRename)
+
+	if wRename.Code != 400 && wRename.Body.String() != "Please pass POST/PUT/GET parameter value of 'old'." {
+		t.Error("Expected error code 400 and message missing parameter old")
+	}
+}
+func TestRenameMissingNewParameter(t *testing.T) {
+	setupTestCase()
+	defer tearDownTestCase()
+	reqCreate := httptest.NewRequest("GET", requestCreate, nil)
+	reqRename := httptest.NewRequest("GET", requestRenameMissingNew, nil)
+
+	w := httptest.NewRecorder()
+	wRename := httptest.NewRecorder()
+
+	var err error
+	if HttpDB, err = db.OpenDB(tempDir); err != nil {
+		panic(err)
+	}
+	Create(w, reqCreate)
+	Rename(wRename, reqRename)
+
+	if wRename.Code != 400 && wRename.Body.String() != "Please pass POST/PUT/GET parameter value of 'new'." {
+		t.Error("Expected error code 400 and message missing parameter new")
 	}
 }
