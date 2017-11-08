f645f894cdde5c7b6c2aed00e30a19dc6fa8790d
agoalofalife <agoalofalife@gmail.com>
2017-11-08 18:19:31 +0300

    add tests crv

--- httpapi/srv_test.go
+++ httpapi/srv_test.go
@@ -1,10 +1,16 @@
 package httpapi
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/HouzuoGuo/tiedot/db"
+	"github.com/bouk/monkey"
+	"github.com/pkg/errors"
+	"log"
+	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -23,7 +29,6 @@ func TestSrc(t *testing.T) {
 	}
 	managerSubTests(testsSrc, "src_test", t)
 }
-
 func TWelcome(t *testing.T) {
 	setupTestCase()
 	defer tearDownTestCase()
@@ -92,4 +97,40 @@ func TRequireTrue(t *testing.T) {
 	if Require(w, req, "test", &test) != true {
 		t.Error("Expected bool true from require function")
 	}
+}
+func TestStartListenAndServe(t *testing.T) {
+	setupTestCase()
+	defer tearDownTestCase()
+	var (
+		str bytes.Buffer
+		s   *http.Server
+	)
+	log.SetOutput(&str)
+	pathSever := monkey.PatchInstanceMethod(reflect.TypeOf(s), "ListenAndServe", func(_ *http.Server) error {
+		return errors.New("Error server")
+	})
+	defer pathSever.Unpatch()
+
+	Start(tempDir, 8000, "", "", "", "", "", "")
+}
+func TestStartListenAndServeTLS(t *testing.T) {
+	setupTestCase()
+	defer tearDownTestCase()
+	var (
+		s   *http.Server
+		str bytes.Buffer
+	)
+	log.SetOutput(&str)
+	errMessage := "error start serve"
+	pathSever := monkey.PatchInstanceMethod(reflect.TypeOf(s), "ListenAndServeTLS", func(_ *http.Server, certFile, keyFile string) error {
+		return errors.New(errMessage)
+	})
+	defer pathSever.Unpatch()
+	defer func() {
+		r := recover()
+		if r == nil && r == fmt.Sprintf("Failed to start HTTPS service - %s", errMessage) {
+			t.Fatal("Did not catch Panicf")
+		}
+	}()
+	Start(tempDir, 8000, "tls", "", "", "", "", "")
 }
