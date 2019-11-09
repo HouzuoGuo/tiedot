9d1c403d6aa548ebcb9a4a93291bd9a7d43d4f37
Arda Güçlü <ardaguclu@gmail.com>
2019-11-09 15:28:42 +0300

    Add error handling for unmarshal for documents

--- db/doc.go
+++ db/doc.go
@@ -185,7 +185,10 @@ func (col *Col) Update(id int, doc map[string]interface{}) error {
 
 	// Done with the collection data, next is to maintain indexed values
 	var original map[string]interface{}
-	json.Unmarshal(originalB, &original)
+	if err = json.Unmarshal(originalB, &original); err != nil {
+		col.db.schemaLock.RUnlock()
+		return err
+	}
 	part.LockUpdate(id)
 	if original != nil {
 		col.unindexDoc(id, original)
@@ -218,7 +221,12 @@ func (col *Col) UpdateBytesFunc(id int, update func(origDoc []byte) (newDoc []by
 		return err
 	}
 	var original map[string]interface{}
-	json.Unmarshal(originalB, &original) // Unmarshal originalB before passing it to update
+	// Unmarshal originalB before passing it to update
+	if err = json.Unmarshal(originalB, &original); err != nil {
+		part.DataLock.Unlock()
+		col.db.schemaLock.RUnlock()
+		return err
+	}
 	docB, err := update(originalB)
 	if err != nil {
 		part.DataLock.Unlock()
